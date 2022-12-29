3596be34675de8a5dd7612718a91ef9828d748a4
FunnyParadox <marioteam65@gmail.com>
2022-12-29 23:50:22 +0100

    update

--- vb.bat
+++
@@ -1,8 +0,0 @@
-@echo off
- set /p FDIR1=<C:\Users\%USERNAME%\AppData\Roaming\Installer_YowaneTeam\install.directory
- set FDIR2=%FDIR1:~45,-12% 
-  cd %FDIR2%
-  md voice
- set FDIR1=%FDIR1:~45,-5% 
-  cd %FDIR1%
-move C:\Users\%USERNAME%\AppData\Roaming\Installer_YowaneTeam\voice\*.rar %FDIR1%
